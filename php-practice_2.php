72eff343ce5be9ec47e87d689887452340f95fb2
ISHIJIMARIKU <riku199607@gmail.com>
2026-03-05 15:41:17 +0900

    Q1tic-tac問題~Q2多次元連想配列 問題１まで

--- php-practice_2.php
+++ php-practice_2.php
@@ -1,9 +1,50 @@
 <?php
 // Q1 tic-tac問題
+echo '1から100までのカウントを開始します '."\n\n";
 
+for ($i = 1; $i <= 100; $i++) 
+{
+  if($i%5 === 0 && $i%4 === 0)
+  {
+    echo 'tic-tac'."\n";
+  }
+  elseif ($i%4 === 0) 
+  {
+    echo 'tic'."\n";
+  }
+  elseif($i%5 === 0)
+  {
+    echo 'tac'."\n";
+  }
+  else 
+  {
+    echo $i . "\n";
+  }
+}
 
 // Q2 多次元連想配列
+$personalInfos = [
+    [
+        'name' => 'Aさん',
+        'mail' => 'aaa@mail.com',
+        'tel'  => '09011112222'
+    ],
+    [
+        'name' => 'Bさん',
+        'mail' => 'bbb@mail.com',
+        'tel'  => '08033334444'
+    ],
+    [
+        'name' => 'Cさん',
+        'mail' => 'ccc@mail.com',
+        'tel'  => '09055556666'
+    ],
+];
 
+// 問題1
+echo $personalInfos[1]['name'].'の電話番号は'.$personalInfos[1]['tel'].'です。'; 
+
+// 問題２
 
 // Q3 オブジェクト-1
 
